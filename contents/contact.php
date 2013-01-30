ce004a35ffde004c0016ad6e1098896e3454d619
Alain <hello@alainbarthelemy.com>
2013-01-30 12:15:17 +0000

    bunch of new feats
    * localStorage for lng
    * refactor of loadContent
    * debug of &lng when on home page
    ...

--- contents/contact.php
+++ contents/contact.php
@@ -7,8 +7,8 @@ function validateForm(){
 	console.log('validate');
 	if(validateEmail(emailValue) && emailValue != "my@email.com"){
 		
-		$.post("contents/contact.php",{email: emailValue}, function(data) {
-			$('#content').html(data);
+		$.post("newsletter-subscribe.php",{email: emailValue}, function(data) {
+			$('#newsletter-content').html(data);
 		});
 		return false;
 	}
@@ -42,42 +42,19 @@ function emailTI_onclick(elmnt){
 	<div id="newsletter-content">
 	<?php
 
-	if(isset($_POST['email'])){
 
-		$email = $_POST['email'];
-		
-		$subscribe_headers .= "MIME-Version: 1.0\n";
-		$subscribe_headers .= "Content-type: text/plain; charset=iso-8859-1\n";
-		$subscribe_headers .= "X-Mailer: php\n";
-		$subscribe_headers .= "From: " . $email . "\n";
-		
-		$subscribe_to = 'hemisphere'.'-subscribe-'.str_replace('@','=',$email).'@'.'alainbarthelemy.com';
-		$subscribe_subject = 'subscribe'; //The email subject 
-		$subscribe_body = 'subscribe'; //The email body 
-
-		mail($subscribe_to, $subscribe_subject, $subscribe_body, $subscribe_headers); 
-	
-		echo "<p>Thank you !</p><p>You will receive an email shortly</br> to confirm your subscription.</p>";
-	}else if(isset($_POST['news_letter_unsubscription'])){
+	if(isset($_POST['news_letter_unsubscription'])){
 			echo 'okaaaaaaaaaaaaaaay';
 	}else if(isset($_GET['sc'])){
-		echo "<p></br>Your subscription is now complete !</br>Thank's again !</p>";
-	
+		echo "<p></br>Your subscription is now complete !</br>Thank you !</p>";
 	}else if(isset($_GET['usc'])){
-		$usc = $_GET['usc'];
-		if($usc == "rq" ){
-			echo "<p>One more step...</p><p>You will receive an email shortly</br> to confirm your unsubscription.</p>";
-		}else if($usc == "scss"){
-			echo "<p></br>You have successfully</br>unsubscribed to the newsletter. ";
-		}
-	
+		echo "<p></br>You have successfully</br>unsubscribed to the newsletter. ";
 	}else{
 		echo	'<div id="newsletter-form-container">';
 		echo 	'<form name="news_letter_subscription"  onsubmit="return validateForm()" >
-					<!--<input type="text" name="emailTI" value="my@email.com" onclick="if(this.value == '."'my@email.com'".')this.value='."''".';">-->
 					<input type="text" name="emailTI" value="my@email.com" onclick="emailTI_onclick(this)">
 
-					<input type="submit" value="Submit">
+					<input type="submit" value="Subscribe">
 				</form>';
 		echo 	'</div>';
 	}
--- the.js
+++ the.js
@@ -1,33 +1,40 @@
-
-//var content,xmlhttp;
-
-// var readyStateCheckInterval = setInterval(function() {
-//     if (document.readyState === "complete") {
-//         clearInterval(readyStateCheckInterval);
-//     	start();
-//     }
-// }, 10);
 var DEFAULT_LOCALE = 'fr';
-var currentLocale  = DEFAULT_LOCALE;
+var localStorageLocale = '';
+var HEMISPHERE_GREEN = '#8ACDA3';
+var currentLocale  = '';
 var urlParamsHM = {};
+
+var NO_LOCAL_STRG = false;
+
+
 
 $("document").ready(function (){
 	start();
 });
 
+
 function start(){
+	
+	// before anything else we test the availables features :
+	if(!Modernizr.canvas){
+		console.log('no canvas');	
+	}
+	
+	if(!Modernizr.localstorage){
+		console.log('no localStorage');
+		NO_LOCAL_STRG = true;
+	}
+	
+	var localStorageLocale = localStorage.getItem('hemisp_lng');
+	
+	if((localStorageLocale === undefined) || (localStorageLocale === null))
+		$('#menu_opts').load('menu_'+DEFAULT_LOCALE+'.html');
+	else
+		$('#menu_opts').load('menu_'+localStorageLocale+'.html');
+	
+	
 	$(window).bind('hashchange',onHashChange);
-	urlParamsHM = extractUrlParams();
-	if(urlParamsHM['lng'])
-		currentLocale = urlParamsHM['lng'];	
-	if(window.location.hash != '')
-		$(window).hashchange();	
-}
-
-
-function onHashChange(){
-	console.log(window.location.hash);
-	loadContent(window.location.href);
+	$(window).hashchange();	
 }
 
 
@@ -55,27 +62,33 @@ function langToggleClick(event){
 			console.log('err#1 unexpected langToggle id');
 			break;
 		}
-
 }
+
 //change the url's locale parameter according to the input value
 function setLocale(value){
+	
+	if(!NO_LOCAL_STRG){
+		localStorage.hemisp_lng = value;	
+	}
 	
 	urlParamsHM['lng'] = value;
 	updateParamsInURL();
 }
 
+// takes the urlParamsHM HashMap and write the params in the url
 function updateParamsInURL(){
 	var paramsIndex = window.location.href.indexOf('?')+1;
 	var paramsString = urlParamsHMToString();
-	// for(var param in urlParamsHM){
-	// 	paramsString += param + '=' + urlParamsHM[param] + '&';
-	// }
-	
-	//paramsString = paramsString.substring(0,paramsString.length - 1);
-	if(paramsIndex != 0){
+
+	if(paramsIndex != 0){// we already have params in the url
 		window.location.href = window.location.href.substring(0,paramsIndex)+paramsString;
-	}else{
-		window.location.href += '?'+paramsString;
+	}else{//we don't have any params in the url
+		var hashIndex = window.location.href.indexOf('!#');
+		if(hashIndex != -1){//we have a hashbang in the url
+			window.location.href += '?'+paramsString;
+		}else{// we don't have any hashbang in the url
+			window.location.href += '#!?'+paramsString;	
+		}
 	}
 }
 
@@ -89,29 +102,86 @@ function urlParamsHMToString(){
 }
 
 
+
+function onHashChange(){
+	//console.log(window.location.hash);
+	
+	loadContent(window.location.href);
+	
+	urlParamsHM = extractUrlParams();
+	updateLangIfRequiered();
+}
+
+
+// loadContent is designed to load content inside the #content element
 function loadContent(url){
 	console.log(url);
-	//history.pushState(null,null,url)
+	
 	var hashPos = url.indexOf('#!'); 
 	if(hashPos != -1){
+		if(url.indexOf('?') !=-1 && url.indexOf('?') == hashPos+2){
+			// in this case we have nohin to load in #content
+			$("#content").html('');
+			return;	
+		}
 		
 		var newUrl = url.substring(0,hashPos) + url.substring(hashPos+2);
 		url = newUrl;		
+	}else{
+		//nohin to load in #content	
+		$("#content").html('');
+		return;
 	}
+	
 	$("#content").load(url);
+	
+	
 }
 
+function updateLangIfRequiered(){
+	
+	if(urlParamsHM['lng']){
+		
+		if(currentLocale != urlParamsHM['lng']){
+			currentLocale = urlParamsHM['lng'];
+			$('#menu_opts').load('menu_'+currentLocale+'.html');
+			switch (currentLocale){
+				case 'en': 			
+					$('#langToggle_en').css('color',HEMISPHERE_GREEN);
+					$('#langToggle_fr').css('color','');	
+					break;
+				case 'fr': 			
+					$('#langToggle_fr').css('color',HEMISPHERE_GREEN);
+					$('#langToggle_en').css('color','');	
+					break;
+				default:
+					console.log('err#2 locale not supported');
+				
+			}
+			
+		}
+	}else{
+		$('#langToggle_fr').css('color','');
+		$('#langToggle_en').css('color','');
+	}
+	
+}
+
+
+
+//toggleFolded is called to fold and unfold subcontent
 function toggleFold(elmnt){
     	 $(elmnt).next(".subcontent").slideToggle(300, function() {
 	  		  // Animation complete.
     	 });
 }
 
-
+// extractUrlParams takes all url variables and puts them in an hashmap (object)
+// we can't use here window.location.search because of the !# 
 function extractUrlParams(){	
 	var t = [];
-	if(window.location.search != "")
-		t = location.search.substring(1).split('&');
+	if(window.location.href.indexOf('?') != -1)
+		t = window.location.href.substring(window.location.href.indexOf('?')+1).split('&');
 	var f = {};
 	for (var i=0; i<t.length; i++){
 		var x = t[ i ].split('=');
